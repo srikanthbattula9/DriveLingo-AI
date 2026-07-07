078874b3c7e71d2d5c1b9bf68ae553b853b346c8
srikanthbattula9 <srikb5899@gmail.com>
2026-07-06 22:43:39 -0400

    Load database URL from environment

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://srikanth@localhost/drivelingo"
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
